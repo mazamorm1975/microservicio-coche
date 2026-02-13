9e5074cb37b8fbc5ba40330e78d872e9ef009511
Mario <mzamora114@hotmail.com>
2026-02-13 11:59:48 -0600

    Commit 13022026

--- src/main/java/com/coche/controller/CarController.java
+++ src/main/java/com/coche/controller/CarController.java
@@ -25,52 +25,48 @@ public class CarController {
 	@Autowired
 	private CarService carService;
 
-	//Api Rest para ingresar registro de automovil a la base de datos microservice_coche
+	// Api Rest para ingresar registro de automovil a la base de datos
+	// microservice_coche
 	@PostMapping("/ingresar")
-	public ResponseEntity<Carro> registrarVehiculo(@RequestBody Carro carbody){
-				
-		Carro brandNewCar = carService.registrar(carbody);	
-				
+	public ResponseEntity<Carro> registrarVehiculo(@RequestBody Carro carbody) {
+
+		Carro brandNewCar = carService.registrar(carbody);
+
 		return new ResponseEntity<Carro>(brandNewCar, HttpStatus.CREATED);
 	}
 
-	
-	//Api Rest para ubicar por id del coche de la base de datos microservice_coche
+	// Api Rest para ubicar por id del coche de la base de datos microservice_coche
 	@GetMapping("/{id}")
-	public ResponseEntity<List<Carro>> buscarPorId(@PathVariable(ID) int id){
-		
+	public ResponseEntity<List<Carro>> buscarPorId(@PathVariable(ID) int id) {
+
 		Carro car = carService.busquedaPorId(id);
-		
- 		if(car != null) {
-		 
-		//Se convierte a lista lo que devuelve el metodo busquedaPorId en el que se implementa un optional	
-		List<Carro> car2 = Arrays.asList(carService.busquedaPorId(id));
-		
-		  return new ResponseEntity<List<Carro>>(car2, HttpStatus.OK);
+
+		if (car != null) {
+
+			// Se convierte a lista lo que devuelve el metodo busquedaPorId en el que se
+			// implementa un optional
+			List<Carro> car2 = Arrays.asList(carService.busquedaPorId(id));
+
+			return new ResponseEntity<List<Carro>>(car2, HttpStatus.OK);
 		}
-		
+
 		return new ResponseEntity<List<Carro>>(HttpStatus.NOT_FOUND);
 	}
-	
-	
-	
-	//Api Rest para obtener el listado general de TODOS los automoviles de la base de datos microservice_coche
+
+	// Api Rest para obtener el listado general de TODOS los automoviles de la base
+	// de datos microservice_coche
 	@GetMapping("/busqueda_general")
-	public ResponseEntity<List<Carro>> listadoCompleto(){
-		
+	public ResponseEntity<List<Carro>> listadoCompleto() {
 		List<Carro> listaTotalInventario = carService.listadoTodosLosCarros();
 		return new ResponseEntity<List<Carro>>(listaTotalInventario, HttpStatus.OK);
 	}
-	
-	
+
 	@GetMapping("/busquedaPorUsuarioId/{usuarioId}")
-	public ResponseEntity<List<Carro>> listadoCarrosPorUsuarioId(@PathVariable("usuarioId") int usuarioId){
-		
+	public ResponseEntity<List<Carro>> listadoCarrosPorUsuarioId(@PathVariable("usuarioId") int usuarioId) {
+
 		List<Carro> listaTodosLosCarrosDeUnUsuario = carService.busquedaPorUsuarioId(usuarioId);
-		
-	return new ResponseEntity<List<Carro>>(listaTodosLosCarrosDeUnUsuario, HttpStatus.OK);
+
+		return new ResponseEntity<List<Carro>>(listaTodosLosCarrosDeUnUsuario, HttpStatus.OK);
 	}
-	
-	
-	
+
 }
